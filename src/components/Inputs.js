c374b031eeb78763717bbccd7734cc7fc7418d45
federicomoscardo <federico.moscardo@gmail.com>
2021-07-12 17:39:29 -0300

    created the structure of this component

---
+++ src/components/Inputs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Inputs extends Component {
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Imputs
