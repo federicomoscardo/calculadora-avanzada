37d2b493f682d3c6e419e79409e3269e10dcff7a
federicomoscardo <federico.moscardo@gmail.com>
2021-07-12 18:02:52 -0300

    added inputs and buttons

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 
 class Inputs extends Component {
-    
+
     render() {
         return (
             <div>
-                
+                <input></input>
+                <button>Clear</button>
+
+                <input></input>
+                <button>Clear</button>
             </div>
         )
     }
 }
 
-export default Imputs
+export default Inputs
