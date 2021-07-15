be2f780f3303431d18a170aedb9704117cdb2f51
federicomoscardo <federico.moscardo@gmail.com>
2021-07-15 18:49:22 -0300

    added <p> elements to show result of pow, sqrt and hypotenuse

--- src/components/Results.js
+++ src/components/Results.js
@@ -7,7 +7,11 @@ function Results(props) {
             <p>Addition: {Number(props.input1) + Number(props.input2)} </p>
             <p>Subtraction: {Number(props.input1) - Number(props.input2)} </p>
             <p>Multiplication: {Number(props.input1) * Number(props.input2)} </p>
-            <p>Division: {Number(props.input2) === 0 ? 0 : Number(props.input1) / Number(props.input2)} </p>
+            <p>Division: {Number(props.input2) === 0 ? 'Math Error!' : Number(props.input1) / Number(props.input2)} </p>
+            <p>Pow: {Math.pow(Number(props.input1), Number(props.input2))} </p>
+            <p>Sqrt Input1: {Math.sqrt(Number(props.input1))} </p>
+            <p>Sqrt Input2: {Math.sqrt(Number(props.input2))} </p>
+            <p>Hypotenuse: {Math.sqrt(Math.pow(Number(props.input1), 2) + Math.pow(Number(props.input2), 2))} </p>
         </div>
     )
 }
