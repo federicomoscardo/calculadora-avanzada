3576335b584d1ade7f0eac963cbd7b8f4e4ff606
federicomoscardo <federico.moscardo@gmail.com>
2021-07-15 17:43:21 -0300

    chenged the component Inputs to Input and added the basic CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Inputs from './components/Inputs'
+import Input from './components/Input'
 import Results from './components/Results'
 
 class App extends Component {
@@ -9,12 +9,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Inputs></Inputs>
-
-        <Results></Results>
+        <div className="d-flex">
+          <Input numInput='1' ></Input>
+          <Input numInput='2' ></Input>
+        </div>
+          <Results></Results>
       </div>
     )
   }
 }
 
-export default App;
+
+
+export default App
